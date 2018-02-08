64da1ec09956be8d319863d7e4a832a007d782dd
Matt Styles <matthew.styles@informed.com>
2018-02-08 17:25:56 +0000

    removed auth from FE, and created login for arc review

---
+++ application/models.py
@@ -0,0 +1,387 @@
+from __future__ import unicode_literals
+
+from uuid import uuid4
+
+from django.db import models
+
+from django.forms import DateTimeField
+
+
+class ArcReview(models.Model):
+    application_id = models.UUIDField(primary_key=True, default=uuid4)
+    user_id = models.CharField(max_length=50)
+    last_accessed = DateTimeField()
+    date_submitted = DateTimeField()
+    applicant_name = models.CharField(max_length=50)
+    app_type = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'ARC_REVIEW'
+
+
+class AdultInHome(models.Model):
+    adult_id = models.UUIDField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    middle_names = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_day = models.IntegerField()
+    birth_month = models.IntegerField()
+    birth_year = models.IntegerField()
+    relationship = models.CharField(max_length=100)
+    dbs_certificate_number = models.CharField(max_length=50)
+    application = models.ForeignKey('Application', models.DO_NOTHING)
+    adult = models.IntegerField(blank=True, null=True)
+    permission_declare = models.NullBooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'ADULT_IN_HOME'
+
+
+class ApplicantHomeAddress(models.Model):
+    home_address_id = models.UUIDField(primary_key=True)
+    street_line1 = models.CharField(max_length=100)
+    street_line2 = models.CharField(max_length=100)
+    town = models.CharField(max_length=100)
+    county = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=8)
+    childcare_address = models.NullBooleanField()
+    current_address = models.NullBooleanField()
+    move_in_month = models.IntegerField()
+    move_in_year = models.IntegerField()
+    personal_detail = models.ForeignKey('ApplicantPersonalDetails', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'APPLICANT_HOME_ADDRESS'
+
+
+class ApplicantName(models.Model):
+    name_id = models.UUIDField(primary_key=True)
+    current_name = models.BooleanField()
+    first_name = models.CharField(max_length=100)
+    middle_names = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    personal_detail = models.ForeignKey('ApplicantPersonalDetails', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'APPLICANT_NAME'
+
+
+class ApplicantPersonalDetails(models.Model):
+    personal_detail_id = models.UUIDField(primary_key=True)
+    birth_day = models.IntegerField(blank=True, null=True)
+    birth_month = models.IntegerField(blank=True, null=True)
+    birth_year = models.IntegerField(blank=True, null=True)
+    application = models.ForeignKey('Application', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'APPLICANT_PERSONAL_DETAILS'
+
+
+class Application(models.Model):
+    application_id = models.UUIDField(primary_key=True)
+    application_type = models.CharField(max_length=50)
+    application_status = models.CharField(max_length=50)
+    cygnum_urn = models.CharField(max_length=50)
+    login_details_status = models.CharField(max_length=50)
+    personal_details_status = models.CharField(max_length=50)
+    childcare_type_status = models.CharField(max_length=50)
+    first_aid_training_status = models.CharField(max_length=50)
+    eyfs_training_status = models.CharField(max_length=50)
+    criminal_record_check_status = models.CharField(max_length=50)
+    health_status = models.CharField(max_length=50)
+    references_status = models.CharField(max_length=50)
+    people_in_home_status = models.CharField(max_length=50)
+    declarations_status = models.CharField(max_length=50)
+    date_created = models.DateTimeField(blank=True, null=True)
+    date_updated = models.DateTimeField(blank=True, null=True)
+    date_accepted = models.DateTimeField(blank=True, null=True)
+    order_code = models.UUIDField(blank=True, null=True)
+    login = models.ForeignKey('UserDetails', models.DO_NOTHING, blank=True, null=True)
+    adults_in_home = models.NullBooleanField()
+    children_in_home = models.NullBooleanField()
+    children_turning_16 = models.NullBooleanField()
+    background_check_declare = models.NullBooleanField()
+    eyfs_questions_declare = models.NullBooleanField()
+    inspect_home_declare = models.NullBooleanField()
+    interview_declare = models.NullBooleanField()
+    information_correct_declare = models.NullBooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'APPLICATION'
+
+
+class ChildcareType(models.Model):
+    childcare_id = models.UUIDField(primary_key=True)
+    zero_to_five = models.BooleanField()
+    five_to_eight = models.BooleanField()
+    eight_plus = models.BooleanField()
+    application = models.ForeignKey(Application, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'CHILDCARE_TYPE'
+
+
+class ChildInHome(models.Model):
+    child_id = models.UUIDField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    middle_names = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_day = models.IntegerField()
+    birth_month = models.IntegerField()
+    birth_year = models.IntegerField()
+    relationship = models.CharField(max_length=100)
+    application = models.ForeignKey(Application, models.DO_NOTHING)
+    child = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'CHILD_IN_HOME'
+
+
+class CriminalRecordCheck(models.Model):
+    criminal_record_id = models.UUIDField(primary_key=True)
+    dbs_certificate_number = models.CharField(max_length=50)
+    cautions_convictions = models.BooleanField()
+    send_certificate_declare = models.NullBooleanField()
+    application = models.ForeignKey(Application, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'CRIMINAL_RECORD_CHECK'
+
+
+class Eyfs(models.Model):
+    eyfs_id = models.UUIDField(primary_key=True)
+    eyfs_understand = models.NullBooleanField()
+    eyfs_training_declare = models.NullBooleanField()
+    eyfs_questions_declare = models.NullBooleanField()
+    application = models.ForeignKey(Application, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'EYFS'
+
+
+class FirstAidTraining(models.Model):
+    first_aid_id = models.UUIDField(primary_key=True)
+    training_organisation = models.CharField(max_length=100)
+    course_title = models.CharField(max_length=100)
+    course_day = models.IntegerField()
+    course_month = models.IntegerField()
+    course_year = models.IntegerField()
+    application = models.ForeignKey(Application, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'FIRST_AID_TRAINING'
+
+
+class Hdb(models.Model):
+    hdb_id = models.UUIDField(primary_key=True)
+    send_hdb_declare = models.NullBooleanField()
+    application = models.ForeignKey(Application, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'HDB'
+
+
+class Reference(models.Model):
+    reference_id = models.UUIDField(primary_key=True)
+    reference = models.IntegerField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    relationship = models.CharField(max_length=100)
+    years_known = models.IntegerField()
+    months_known = models.IntegerField()
+    street_line1 = models.CharField(max_length=100)
+    street_line2 = models.CharField(max_length=100)
+    town = models.CharField(max_length=100)
+    county = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=8)
+    phone_number = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    application = models.ForeignKey(Application, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'REFERENCE'
+
+
+class UserDetails(models.Model):
+    login_id = models.UUIDField(primary_key=True)
+    email = models.CharField(max_length=100)
+    mobile_number = models.CharField(max_length=20)
+    add_phone_number = models.CharField(max_length=20)
+    email_expiry_date = models.IntegerField(blank=True, null=True)
+    sms_expiry_date = models.IntegerField(blank=True, null=True)
+    magic_link_email = models.CharField(max_length=100, blank=True, null=True)
+    magic_link_sms = models.CharField(max_length=100, blank=True, null=True)
+    security_question = models.CharField(max_length=100, blank=True, null=True)
+    security_answer = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'USER_DETAILS'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=80)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class GovukTemplateBaseLink(models.Model):
+    modified = models.DateTimeField()
+    name = models.CharField(max_length=255)
+    localise_name = models.BooleanField()
+    link = models.CharField(max_length=255)
+    link_is_view_name = models.BooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'govuk_template_base_link'
+
+
+class GovukTemplateBaseServicesettings(models.Model):
+    modified = models.DateTimeField()
+    name = models.CharField(max_length=100)
+    localise_name = models.BooleanField()
+    phase = models.CharField(max_length=10)
+    header_link_view_name = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'govuk_template_base_servicesettings'
+
+
+class GovukTemplateBaseServicesettingsFooterLinks(models.Model):
+    servicesettings = models.ForeignKey(GovukTemplateBaseServicesettings, models.DO_NOTHING)
+    link = models.ForeignKey(GovukTemplateBaseLink, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'govuk_template_base_servicesettings_footer_links'
+        unique_together = (('servicesettings', 'link'),)
+
+
+class GovukTemplateBaseServicesettingsHeaderLinks(models.Model):
+    servicesettings = models.ForeignKey(GovukTemplateBaseServicesettings, models.DO_NOTHING)
+    link = models.ForeignKey(GovukTemplateBaseLink, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'govuk_template_base_servicesettings_header_links'
+        unique_together = (('servicesettings', 'link'),)
---
+++ application/views.py
@@ -0,0 +1,173 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UsernameField, UserModel
+from django.contrib.auth.models import User
+from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.utils.http import urlsafe_base64_decode
+from django.utils.text import capfirst
+from govuk_forms.forms import GOVUKForm
+from django import forms
+from django.contrib.auth import login as auth_login
+
+from .models import Application, ArcReview
+
+
+@login_required()
+def summary_page(request):
+    apps = get_assigned_apps(request)
+    if request.method == 'POST':
+        assign_new_application
+    return render(request, './summary.html')
+
+
+@login_required()
+def assign_new_application(request):
+    application_id = get_oldest_application_id()
+    print(application_id)
+    # ArcReview.objects.all().delete()
+    arc_user = ArcReview.objects.create()
+    arc_user.user_id = request.user.id
+    arc_user.application_id = application_id
+    arc_user.date_submitted = ArcReview.objects.filter(pk=application_id)
+    arc_user.name = get_name(arc_user.application_id)
+    arc_user.app_type = 'childminder'
+    arc_user.save()
+    print("saved")
+    return JsonResponse({'message': arc_user.application_id})
+
+
+@login_required()
+def delete_all(request):
+    try:
+        ArcReview.objects.all().delete()
+    except Exception as ex:
+        HttpResponse(ex)
+
+
+def get_assigned_apps(request):
+    apps = ArcReview.objects.all()
+    print(apps)
+    arr = []
+    for i in apps:
+        print("ID: " + i.user_id)
+        arr.append(i)
+    return arr
+
+
+def get_name(application_id):
+    # lookup app id, traverse throught tables and return name
+    name = ''
+    return name
+
+
+def get_oldest_application_id():
+    application_list = Application.objects.all().order_by('date_created')
+    for i in application_list:
+        if len(ArcReview.objects.filter(pk=i.application_id)) == 0:
+            return i.application_id
+    # if it gets to here return error, no application for review
+
+def get_users():
+    users = User.objects.all()
+    return users
+
+
+def login(request):
+    form = AuthenticationForm()
+    variables = {
+        'form': form
+    }
+    if request.method == 'POST':
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return HttpResponseRedirect('/summary')
+            else:
+                return HttpResponse("Not signed in")
+        except Exception as ex:
+            print(ex)
+
+    return render(request, './registration/login.html', variables)
+
+
+def get_user(self, uidb64):
+    try:
+        # urlsafe_base64_decode() decodes to bytestring
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = UserModel._default_manager.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
+        user = None
+    return user
+
+
+class AuthenticationForm(GOVUKForm):
+    """
+    Base class for authenticating users. Extend this to get a form that accepts
+    username/password logins.
+    """
+    field_label_classes = 'form-label-bold'
+    auto_replace_widgets = True
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, request=None, *args, **kwargs):
+        """
+        The 'request' parameter is set for custom auth use by subclasses.
+        The form data comes in via the standard 'data' kwarg.
+        """
+        self.request = request
+        self.user_cache = None
+        super().__init__(*args, **kwargs)
+
+        # Set the max length and label for the "username" field.
+        self.username_field = UserModel._meta.get_field(UserModel.USERNAME_FIELD)
+        self.fields['username'].max_length = self.username_field.max_length or 254
+        if self.fields['username'].label is None:
+            self.fields['username'].label = capfirst(self.username_field.verbose_name)
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username is not None and password:
+            self.user_cache = authenticate(self.request, username=username, password=password)
+            if self.user_cache is None:
+                raise self.get_invalid_login_error()
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+        return self.cleaned_data
+
+    def confirm_login_allowed(self, user):
+        """
+        Controls whether the given User may log in. This is a policy setting,
+        independent of end-user authentication. This default behavior is to
+        allow login by active users, and reject login by inactive users.
+        If the given user cannot log in, this method should raise a
+        ``forms.ValidationError``.
+        If the given user may log in, this method should return None.
+        """
+        if not user.is_active:
+            raise forms.ValidationError(
+                self.error_messages['inactive'],
+                code='inactive',
+            )
+
+    def get_user_id(self):
+        if self.user_cache:
+            return self.user_cache.id
+        return None
+
+    def get_user(self):
+        return self.user_cache
+
+    def get_invalid_login_error(self):
+        return forms.ValidationError(
+            self.error_messages['invalid_login'],
+            code='invalid_login',
+            params={'username': self.username_field.verbose_name},
+        )
