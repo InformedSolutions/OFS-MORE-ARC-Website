7684767e9719f0aefc8bccba553ece6a14faa9c4
Matt Styles <matthew.styles@informed.com>
2018-02-09 11:31:44 +0000

    added migrate to pipeline and whitenoise

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+
+# Create database migration files
+echo "Create database migration files"
+python manage.py makemigrations
+python manage.py makemigrations application
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+python manage.py migrate application
+
+#Collect static resources
+echo "Collecting static assets"
+mkdir static
+python manage.py collectstatic --noinput
+
+
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@admin.com').delete(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
-
-
 
 # Start server
 echo "Starting server"
