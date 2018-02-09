e8796ce3a3d8736b5756d354e3cb979896d9e11f
Matt Styles <matthew.styles@informed.com>
2018-02-09 11:27:05 +0000

    mapped settingsfile and create superuser

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@admin.com').delete(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+
+
+
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
