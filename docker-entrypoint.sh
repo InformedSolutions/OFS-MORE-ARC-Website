37ba4e371bb95894aa94af8e9fb9fa40b0b7319e
Cruddas <james.cruddas@informed.com>
2018-02-27 08:27:23 +0000

    arc: docker-entrypoint.sh: Fix collectstatic command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,7 @@ python manage.py migrate arc_application --settings=$PROJECT_SETTINGS
 #Collect static resources
 echo "Collecting static assets"
 mkdir static
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --settings=$PROJECT_SETTINGS
 
 echo "from django.contrib.auth.models import User; User.objects.filter(username='root').delete(); User.objects.create_superuser('root', 'root@admin.com', 'default-password')" | python manage.py shell --settings=$PROJECT_SETTINGS
 
