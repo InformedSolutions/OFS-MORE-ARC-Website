094bc76b3bfde7ccbe05fa35c7390e4d3e6081ff
Cruddas <james.cruddas@informed.com>
2018-02-28 09:48:13 +0000

    settings: Fix static assets folder and base directory

--- arc_service/settings/prod.py
+++ arc_service/settings/prod.py
@@ -5,6 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 PROD_APPS = [
+    'whitenoise',
 ]
 
 INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROD_APPS + PROJECT_APPS
@@ -12,13 +13,24 @@ INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROD_APPS + PROJECT_APPS
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': os.environ.get('DATABASE_USER', 'ofsted'),
+#         'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'OfstedB3ta'),
+#         'HOST': os.environ.get('DATABASE_HOST', 'ofsted-postgres'),
+#         'PORT': os.environ.get('DATABASE_PORT', '5432')
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': os.environ.get('DATABASE_USER', 'ofsted'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'OfstedB3ta'),
-        'HOST': os.environ.get('DATABASE_HOST', 'ofsted-postgres'),
-        'PORT': os.environ.get('DATABASE_PORT', '5432')
+        'HOST': os.environ.get('DATABASE_HOST', '130.130.52.132'),
+        'PORT': os.environ.get('DATABASE_PORT', '5462')
     }
 }
