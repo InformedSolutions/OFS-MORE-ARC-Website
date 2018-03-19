39b1ec8100f87514978450db1f94436b6e8a87e9
GeevesH-IS <hugo.geeves@informed.com>
2018-03-19 16:54:02 +0000

    magic_link.py: Add exception logging on all caught errors
    As specified in ticket [CCN3-660] and subticket [CCN3-735]

--- arc_service/settings/dev.py
+++ arc_service/settings/dev.py
@@ -25,3 +25,33 @@ DATABASES = {
         'PORT': os.environ.get('POSTGRES_PORT', '5462')
     }
 }
+
+# Automatic Django logging at the INFO level (i.e everything the comes to the console when ran locally)
+LOGGING = {
+  'version': 1,
+  'disable_existing_loggers': False,
+  'formatters': {
+    'console': {
+            # exact format is not important, this is the minimum information
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        },
+        },
+  'handlers': {
+    'django.server': {
+        'level': 'INFO',
+        'class': 'logging.handlers.RotatingFileHandler',
+        'maxBytes': 1 * 1024 * 1024,
+        'filename': 'logs/output.log',
+        'formatter': 'console',
+        'maxBytes': 1 * 1024 * 1024,
+        'backupCount': '30'
+    },
+   },
+   'loggers': {
+     'django.server': {
+       'handlers': ['django.server'],
+         'level': 'INFO',
+           'propagate': True,
+      },
+    },
+}
