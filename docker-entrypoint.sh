3f54ae342a03a75038c7885e93024627cf766bd3
James Cruddas <james.cruddas@informed.com>
2018-05-01 23:47:48 +0100

    Temporarily fake initial migration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,10 @@
 # Create database migration files
 echo "Create database migration files"
 python manage.py makemigrations
-python manage.py makemigrations arc_application
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py migrate --settings=$PROJECT_SETTINGS
-python manage.py migrate arc_application --settings=$PROJECT_SETTINGS
+python manage.py migrate --fake-initial --settings=$PROJECT_SETTINGS
 
 #Collect static resources
 echo "Collecting static assets"
