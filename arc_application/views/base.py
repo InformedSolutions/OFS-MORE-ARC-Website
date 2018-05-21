16b5b5804b50da51ca1f0084fe4de0cb73534e00
Alice King <alice.king@informed.com>
2018-05-21 11:22:18 +0100

    mixed content changes

--- arc_application/views/base.py
+++ arc_application/views/base.py
@@ -328,6 +328,8 @@ class AuditlogListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(AuditlogListView, self).get_context_data(**kwargs)
+        app_id = self.request.GET.get('id')
+        context['orderCode'] = Application.objects.get(application_id=app_id).order_code
 
         if has_group(self.request.user, settings.CONTACT_CENTRE):
             context['back'] = reverse('search')
