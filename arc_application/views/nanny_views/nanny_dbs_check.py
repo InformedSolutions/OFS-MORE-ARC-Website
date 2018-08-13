1fbba8b12108c9007cfee0222a514e55bc4cfa54
vagrant <thomas.stokes@informed.com>
2018-08-13 20:07:28 +0100

    nanny_dbs_check.py: Update for NannyARCFormView.

--- arc_application/views/nanny_views/nanny_dbs_check.py
+++ arc_application/views/nanny_views/nanny_dbs_check.py
@@ -1,15 +1,13 @@
 from arc_application.services.db_gateways import NannyGatewayActions
-
-from arc_application.forms.nanny_forms.nanny_form_builder import dbs_form
-
+from arc_application.forms.nanny_forms.nanny_form_builder import DBSForm
 from arc_application.views.nanny_views.nanny_form_view import NannyARCFormView
 
 
 class NannyDbsCheckSummary(NannyARCFormView):
     template_name = 'nanny_general_template.html'
-    success_url = 'nanny_task_list'  # TODO - Update to 'nanny_insurance_cover_summary' once functional.
+    success_url = 'nanny_insurance_cover_summary'
     task_for_review = 'dbs_review'
-    form = dbs_form
+    form_class = DBSForm
 
     def get_context_data(self, application_id):
         """
@@ -24,25 +22,26 @@ class NannyDbsCheckSummary(NannyARCFormView):
         dbs_certificate_number = dbs_check_dict['dbs_number']
         criminal_bool = dbs_check_dict['convictions']
 
-        # Set up context
+        form = self.get_form()
+
         context = {
             'application_id': application_id,
             'title': 'Review: Criminal record (DBS) check',
-            'form': dbs_form,
+            'form': form,
             'rows': [
                 {
                     'id': 'dbs_number',
                     'name': 'DBS certificate number',
                     'info': dbs_certificate_number,
-                    'declare': dbs_form['dbs_number_declare'],
-                    'comments': dbs_form['dbs_number_comments'],
+                    'declare': form['dbs_number_declare'],
+                    'comments': form['dbs_number_comments'],
                 },
                 {
                     'id': 'convictions',
                     'name': 'Do you have any criminal cautions or convictions?',
                     'info': criminal_bool,
-                    'declare': dbs_form['convictions_declare'],
-                    'comments': dbs_form['convictions_comments'],
+                    'declare': form['convictions_declare'],
+                    'comments': form['convictions_comments'],
                 }
             ]
         }
