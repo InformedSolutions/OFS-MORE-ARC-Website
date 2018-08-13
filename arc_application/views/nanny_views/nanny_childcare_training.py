1ae5721b63dcdc06f1bdeb49fb8bff991ce7550b
vagrant <thomas.stokes@informed.com>
2018-08-13 20:07:51 +0100

    nanny_childcare_training.py: Update for NannyARCFormView.

--- arc_application/views/nanny_views/nanny_childcare_training.py
+++ arc_application/views/nanny_views/nanny_childcare_training.py
@@ -1,54 +1,20 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views import View
-from django.utils.decorators import method_decorator
-
+from arc_application.forms.nanny_forms.nanny_form_builder import ChildcareTrainingForm
 from arc_application.services.db_gateways import NannyGatewayActions
-from arc_application.models import Arc
-from arc_application.review_util import build_url
-from arc_application.forms.nanny_forms.nanny_form_builder import childcare_training_form
+from arc_application.views.nanny_views.nanny_form_view import NannyARCFormView
 
 
+class NannyChildcareTrainingSummary(NannyARCFormView):
+    template_name = 'nanny_general_template.html'
+    success_url = 'nanny_dbs_summary'
+    task_for_review = 'childcare_training_review'
+    form_class = ChildcareTrainingForm
 
-@method_decorator(login_required, name='get')
-@method_decorator(login_required, name='post')
-class NannyChildcareTrainingSummary(View):
-    TEMPLATE_NAME = 'nanny_general_template.html'
-    FORM_NAME = ''
-    REDIRECT_NAME = 'nanny_dbs_summary'
-
-    def get(self, request):
-
-        # Get application ID
-        application_id = request.GET["id"]
-
-        context = self.create_context(application_id)
-
-        return render(request, self.TEMPLATE_NAME, context=context)
-
-    def post(self, request):
-
-        # Get application ID
-        application_id = request.POST["id"]
-
-        # Update task status to COMPLETED
-        arc_application = Arc.objects.get(application_id=application_id)
-        arc_application.childcare_training_review = 'COMPLETED'
-        arc_application.save()
-
-        redirect_address = build_url(self.REDIRECT_NAME, get={'id': application_id})
-
-        return HttpResponseRedirect(redirect_address)
-
-    def create_context(self, application_id):
+    def get_context_data(self, application_id):
         """
         Creates the context dictionary for this view.
         :param application_id: Reviewed application's id.
         :return: Context dictionary.
         """
-
-        # Get nanny information
         nanny_actions = NannyGatewayActions()
         childcare_training_dict = nanny_actions.read('childcare-training',
                                                      params={'application_id': application_id}).record
@@ -56,28 +22,28 @@ class NannyChildcareTrainingSummary(View):
         level_2_training = childcare_training_dict['level_2_training']
         common_core_training = childcare_training_dict['common_core_training']
 
-        # Set up context
+        form = self.get_form()
+
         context = {
             'application_id': application_id,
             'title': 'Review: Childcare training',
-            'form': childcare_training_form,
+            'form': form,
             'rows': [
                 {
                     'id': 'level_2_training',
                     'name': 'Do you have a childcare qualification?',
                     'info': level_2_training,
-                    'declare': childcare_training_form['level_2_training_declare'],
-                    'comments': childcare_training_form['level_2_training_comments'],
+                    'declare': form['level_2_training_declare'],
+                    'comments': form['level_2_training_comments'],
                 },
                 {
                     'id': 'common_core_training',
                     'name': 'Have you had common core training?',
                     'info': common_core_training,
-                    'declare': childcare_training_form['common_core_training_declare'],
-                    'comments': childcare_training_form['common_core_training_comments'],
+                    'declare': form['common_core_training_declare'],
+                    'comments': form['common_core_training_comments'],
                 }
             ]
-
         }
 
         return context
