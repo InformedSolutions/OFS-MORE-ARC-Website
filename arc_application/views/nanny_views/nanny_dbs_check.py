7a650fbac684df8b6f35524180024ffde213ab32
vagrant <thomas.stokes@informed.com>
2018-08-13 10:39:13 +0100

    nanny_nanny_check.py: Update dbs_review for NannyARCFormView use.

--- arc_application/views/nanny_views/nanny_dbs_check.py
+++ arc_application/views/nanny_views/nanny_dbs_check.py
@@ -1,56 +1,25 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views import View
-from django.utils.decorators import method_decorator
-
 from arc_application.services.db_gateways import NannyGatewayActions
+
 from arc_application.forms.nanny_forms.nanny_form_builder import dbs_form
-from arc_application.models import Arc
-from arc_application.review_util import build_url
+
+from arc_application.views.nanny_views.nanny_form_view import NannyARCFormView
 
 
-@method_decorator(login_required, name='get')
-@method_decorator(login_required, name='post')
-class NannyDbsCheckSummary(View):
-    TEMPLATE_NAME = 'nanny_general_template.html'
-    FORM_NAME = ''
-    REDIRECT_NAME = 'nanny_insurance_cover_summary'
+class NannyDbsCheckSummary(NannyARCFormView):
+    template_name = 'nanny_general_template.html'
+    success_url = 'nanny_task_list'  # TODO - Update to 'nanny_insurance_cover_summary' once functional.
+    task_for_review = 'dbs_review'
+    form = dbs_form
 
-    def get(self, request):
-
-        # Get application ID
-        application_id = request.GET["id"]
-
-        context = self.create_context(application_id)
-
-        return render(request, self.TEMPLATE_NAME, context=context)
-
-    def post(self, request):
-
-        # Get application ID
-        application_id = request.POST["id"]
-
-        # Update task status to COMPLETED
-        arc_application = Arc.objects.get(application_id=application_id)
-        arc_application.dbs_review = 'COMPLETED'
-        arc_application.save()
-
-        redirect_address = build_url(self.REDIRECT_NAME, get={'id': application_id})
-
-        return HttpResponseRedirect(redirect_address)
-
-    def create_context(self, application_id):
+    def get_context_data(self, application_id):
         """
         Creates the context dictionary for this view.
         :param application_id: Reviewed application's id.
         :return: Context dictionary.
         """
-
         # Get nanny information
         nanny_actions = NannyGatewayActions()
-        dbs_check_dict = nanny_actions.read('dbs-check',
-                                            params={'application_id': application_id}).record
+        dbs_check_dict = nanny_actions.read('dbs-check', params={'application_id': application_id}).record
 
         dbs_certificate_number = dbs_check_dict['dbs_number']
         criminal_bool = dbs_check_dict['convictions']
