8e5d84b0d8375a9bc56efad052cb1a60a557a4d8
Rica Duchateau <rica.duchateau@informed.com>
2018-09-26 13:50:26 +0100

    Create Your home and childcare address summary table

--- arc_application/summary_page_data.py
+++ arc_application/summary_page_data.py
@@ -4,7 +4,8 @@ Link dict to associate tables with their corresponding view for change link reve
 link_dict = {
     'Your sign in details': 'contact_summary',
     'Type of childcare': 'type_of_childcare_age_groups',
-    'Your personal details': 'personal_details_summary',
+    'Your name and date of birth': 'personal_details_summary',
+    'Your home and childcare address': 'personal_details_summary',
     'First aid training': 'first_aid_training_summary',
     'Childcare training': 'childcare_training_check_summary',
     'Health declaration booklet': 'health_check_answers',
