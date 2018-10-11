5c05ffdf82bfd860dac5831ad691c7c99e7e16ee
Thomas-95is <thomas.stokes@informed.com>
2018-10-11 13:29:50 +0100

    test_nanny_audit_log.py: Add tests Nanny audit log.

--- arc_application/tests/test_nanny_audit_log.py
+++ arc_application/tests/test_nanny_audit_log.py
@@ -1,10 +1,14 @@
 from unittest import mock, TestCase
+import uuid
 
 from django.conf import settings
 from django.contrib.auth.models import Group, User
 from django.test import Client
+from django.urls import reverse
 
 from .test_utils import side_effect
+
+from arc_application.views.audit_log import AuditlogListView
 
 
 @mock.patch('arc_application.services.db_gateways.NannyGatewayActions.list', side_effect=side_effect)
@@ -32,29 +36,48 @@ class NannyAuditLogTests(TestCase):
         global cc_test_user
         cc_test_user = cls.user
 
+        cls.application_id = str(uuid.uuid4())
+
     def setUp(self):
         self.client.login(username='governor_tARCin', password='my_secret')
 
-    def test_can_render_audit_log_page_as_arc_user(self):
+    def test_can_render_audit_log_page_as_arc_user(self, mock_create, mock_list):
+        response = self.client.get(reverse('auditlog') + '?id=' + self.application_id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.resolver_match.func.__name__, NannyAuditlogListView.__name__)
+
+    def test_can_render_audit_log_page_as_cc_user(self, mock_create, mock_list):
+        self.client.logout()
+        self.client.login(username='cc_test', password='my_secret')
+
+        response = self.client.get(reverse('auditlog') + '?id=' + self.application_id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.resolver_match.func.__name__, NannyAuditlogListView.__name__)
+
+        self.client.logout()
+
+    def test_get_request_to_audit_log_page_as_cc_user_creates_timeline_log(self, mock_create, mock_list):
+        self.client.logout()
+        self.client.login(username='cc_test', password='my_secret')
+
+        self.client.get(reverse('auditlog') + '?id=' + self.application_id)
+
+        self.assertTrue(mock_create.called)
+        self.assertTrue(mock_create.call_args_list, [])
+
+    def test_returning_application_creates_timeline_log(self, mock_create, mock_list):
         self.skipTest('NotImplemented')
 
-    def test_can_render_audit_log_page_as_cc_user(self):
+    def test_accepting_application_creates_timeline_log(self, mock_create, mock_list):
         self.skipTest('NotImplemented')
 
-    def test_get_request_to_audit_log_page_as_cc_user_creates_timeline_log(self):
+    def test_assigning_application_creates_timeline_log(self, mock_create, mock_list):
         self.skipTest('NotImplemented')
 
-    def test_returning_application_creates_timeline_log(self):
+    def test_releasing_application_creates_timeline_log(self, mock_create, mock_list):
         self.skipTest('NotImplemented')
 
-    def test_accepting_application_creates_timeline_log(self):
-        self.skipTest('NotImplemented')
-
-    def test_assigning_application_creates_timeline_log(self):
-        self.skipTest('NotImplemented')
-
-    def test_releasing_application_creates_timeline_log(self):
-        self.skipTest('NotImplemented')
-
-    def test_adding_arc_comment_creates_timeline_log(self):
+    def test_adding_arc_comment_creates_timeline_log(self, mock_create, mock_list):
         self.skipTest('NotImplemented')
