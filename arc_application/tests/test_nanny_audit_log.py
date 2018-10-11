a7990a3b6767e0db566052ded5a9567ae4cc50dc
Thomas-95is <thomas.stokes@informed.com>
2018-10-11 12:00:26 +0100

    test_audit_log.py: Add mocks for gateway responses.

--- arc_application/tests/test_nanny_audit_log.py
+++ arc_application/tests/test_nanny_audit_log.py
@@ -1,10 +1,14 @@
-from unittest import TestCase
+from unittest import mock, TestCase
 
 from django.conf import settings
 from django.contrib.auth.models import Group, User
 from django.test import Client
 
+from .test_utils import side_effect
 
+
+@mock.patch('arc_application.services.db_gateways.NannyGatewayActions.list', side_effect=side_effect)
+@mock.patch('arc_application.services.db_gateways.NannyGatewayActions.create', side_effect=side_effect)
 class NannyAuditLogTests(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -20,8 +24,6 @@ class NannyAuditLogTests(TestCase):
         global arc_test_user
         arc_test_user = cls.user
 
-        cls.client.login(username='governor_tARCin', password='my_secret')
-
         cls.user = User.objects.create_user(
             username='cc_test', email='cc_as_sunday@morning.com', password='my_secret')
         g2 = Group.objects.create(name=settings.CONTACT_CENTRE)
@@ -29,6 +31,9 @@ class NannyAuditLogTests(TestCase):
 
         global cc_test_user
         cc_test_user = cls.user
+
+    def setUp(self):
+        self.client.login(username='governor_tARCin', password='my_secret')
 
     def test_can_render_audit_log_page_as_arc_user(self):
         self.skipTest('NotImplemented')
