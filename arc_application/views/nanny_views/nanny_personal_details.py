e4907899933202787f7cc90448d50435b80146e7
proctorj <james.proctor@informed.com>
2018-10-12 14:22:34 +0100

    Added arc functionality to the 'your children' page functionality and future task

--- arc_application/views/nanny_views/nanny_personal_details.py
+++ arc_application/views/nanny_views/nanny_personal_details.py
@@ -68,6 +68,8 @@ class NannyPersonalDetailsSummary(NannyARCFormView):
 
         lived_abroad = personal_details['lived_abroad']
 
+        your_children = personal_details['your_children']
+
         forms = self.get_forms()
         personal_details_form = forms[0]
         home_address_form = forms[1]
@@ -114,8 +116,9 @@ class NannyPersonalDetailsSummary(NannyARCFormView):
                 {
                     'id': 'your_children',
                     'name': 'Do you have any children of your own under 16?',
-                    'declare': personal_details_form['children_under16'] if hasattr(self, 'request') else '',
-                    'comments': personal_details_form['children_under16_comments'],
+                    'info': your_children,
+                    'declare': personal_details_form['your_children'] if hasattr(self, 'request') else '',
+                    'comments': personal_details_form['your_children_comments'],
 
                 }
             ]
