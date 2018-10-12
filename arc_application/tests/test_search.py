6ccbbc68ec6fa3ce944d908daff95214b5b4f2c7
LRydeard <luke.rydeard@informed.com>
2018-10-12 15:16:14 +0100

    Updated test docstrings

--- arc_application/tests/test_search.py
+++ arc_application/tests/test_search.py
@@ -9,10 +9,17 @@ from arc_application.services.search_service import SearchService
 
 
 class SearchServiceTests(TestCase):
+    """
+    Test case for Nanny search tests.
+    """
 
     @tag('unit')
     @patch('arc_application.services.search_service.SearchService._search_childminders')
     def test_search_only_childminder(self, mock_search_childminders):
+        """
+        Test to see if Childminder is being searched with 'Childminder' application_type.
+        :param mock_search_childminders: Mock for _search_childminders method in SearchService.
+        """
         SearchService.search("", "", "", "", "", 'Childminder')
 
         self.assertTrue(mock_search_childminders.call_count == 1)
@@ -20,6 +27,10 @@ class SearchServiceTests(TestCase):
     @tag('unit')
     @patch('arc_application.services.search_service.SearchService._search_nannies')
     def test_search_only_nannies(self, mock_search_nannies):
+        """
+        Test to see if Nanny is being searched with 'Nanny' application_type.
+        :param mock_search_childminders: Mock for _search_childminders method in SearchService.
+        """
         SearchService.search("", "", "", "", "", 'Nanny')
 
         self.assertTrue(mock_search_nannies.call_count == 1)
@@ -28,6 +39,11 @@ class SearchServiceTests(TestCase):
     @patch('arc_application.services.search_service.SearchService._search_nannies')
     @patch('arc_application.services.search_service.SearchService._search_childminders')
     def test_search_both_childminder_and_nannies(self, mock_search_childminders, mock_search_nannies):
+        """
+        Test to see if Childminder is being searched with 'All' application_type.
+        :param mock_search_childminders: Mock for _search_childminders method in SearchService.
+        :param mock_search_nannies: Mock for _search_nannies method in SearchService.
+        """
         SearchService.search("", "", "", "", "", 'All')
 
         self.assertTrue(mock_search_childminders.call_count == 1)
@@ -35,6 +51,9 @@ class SearchServiceTests(TestCase):
 
     @tag('unit')
     def test_ordering(self):
+        """
+        Test to see if ordering of search_dicts working as expected.
+        """
         mock_search_dict_1 = {
             'audit_link': '/arc/auditlog?id=4c004204-84e1-4f47-b118-c91fbb47a0bf&app_type=Nanny',
             'summary_link': '/arc/search-summary?id=4c004204-84e1-4f47-b118-c91fbb47a0bf',
@@ -104,7 +123,10 @@ class SearchServiceTests(TestCase):
         self.assertEqual(ordered_mock_search_list_reverse, expected_order_list)
 
     @tag('unit')
-    def test_formating(self):
+    def test_formatting(self):
+        """
+        Test to see if formatting of search_lists working as expected.
+        """
         mock_search_list = [{
             'submission_type': 'ACCEPTED',
             'date_submitted': '03/06/2017',
