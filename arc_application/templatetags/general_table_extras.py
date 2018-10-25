ad603a30355d39b4786829070b4e4810e7ab7a0b
Thomas-95is <thomas.stokes@informed.com>
2018-10-25 12:46:04 +0100

    general_table_extras.py: Register filter for indexing list.

--- arc_application/templatetags/general_table_extras.py
+++ arc_application/templatetags/general_table_extras.py
@@ -1,7 +1,9 @@
 from django import template
 from pydoc import locate
 
+
 register = template.Library()
+
 
 @register.filter
 def template_is_instance(value, arg):
@@ -12,7 +14,13 @@ def template_is_instance(value, arg):
     '''
     return isinstance(value, locate(arg))
 
+
 @register.filter(name='template_string_in_list')
 def template_string_in_list(value, arg):
     arg_list = arg.split()
     return value in arg_list
+
+
+@register.filter
+def return_item_by_index(_list, index):
+    return _list[index]
