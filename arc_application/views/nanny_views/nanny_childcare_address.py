8699e12b6156722536d94b86435c9f9ac52b14ab
Thomas-95is <thomas.stokes@informed.com>
2018-10-25 12:47:33 +0100

    WIP: nanny_childcare_addresses.py: Add formset for childcare addresses.

--- arc_application/views/nanny_views/nanny_childcare_address.py
+++ arc_application/views/nanny_views/nanny_childcare_address.py
@@ -1,53 +1,17 @@
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views import View
-from django.utils.decorators import method_decorator
-
+from arc_application.forms.nanny_forms.nanny_form_builder import ChildcareAddressForm, WhereYouWillWorkForm
 from arc_application.services.db_gateways import NannyGatewayActions
-from arc_application.models import Arc
-from arc_application.review_util import build_url
+
+from .nanny_form_view import NannyARCFormView
 
 
-@method_decorator(login_required, name='get')
-@method_decorator(login_required, name='post')
-class NannyChildcareAddressSummary(View):
-    TEMPLATE_NAME = 'nanny_general_template.html'
-    FORM_NAME = ''
-    REDIRECT_NAME = 'nanny_first_aid_training_summary'
+class NannyChildcareAddressSummary(NannyARCFormView):
+    template_name = 'nanny_general_template.html'
+    success_url = 'nanny_first_aid_training_summary'
+    task_for_review = 'childcare_address_review'
+    verbose_task_name = 'Childcare address'
+    form_class = [WhereYouWillWorkForm,]
 
-    def get(self, request):
-
-        # Get application ID
-        application_id = request.GET["id"]
-
-        context = self.create_context(application_id)
-
-        return render(request, self.TEMPLATE_NAME, context=context)
-
-    def post(self, request):
-
-        # Get application ID
-        application_id = request.POST["id"]
-
-        # Update task status to COMPLETED
-        arc_application = Arc.objects.get(application_id=application_id)
-        arc_application.childcare_address_review = 'COMPLETED'
-        arc_application.save()
-
-        redirect_address = build_url(self.REDIRECT_NAME, get={'id': application_id})
-
-        return HttpResponseRedirect(redirect_address)
-
-    def create_context(self, application_id):
-        """
-        Creates the context dictionary for this view.
-        :param application_id: Reviewed application's id.
-        :return: Context dictionary.
-        """
-
-        # Get nanny information
+    def get_context_data(self, application_id):
         nanny_actions = NannyGatewayActions()
         nanny_application = nanny_actions.read('application',
                                                params={'application_id': application_id}).record
@@ -70,16 +34,40 @@ class NannyChildcareAddressSummary(View):
         else:
             home_address_locations = {}
 
-        # Set up context
+        where_you_will_work_form = WhereYouWillWorkForm()
+
+        from django.forms import formset_factory
+
+        ChildcareAddressFormset = formset_factory(ChildcareAddressForm, extra=0)
+        childcare_address_formset = ChildcareAddressFormset(
+            data={
+                'form-TOTAL_FORMS': str(len(home_address_locations)),
+                'form-INITIAL_FORMS': str(len(home_address_locations)),
+                'form-MAX_NUM_FORMS': str(len(home_address_locations)),
+            }
+        )
+
+        for index, form in enumerate(childcare_address_formset):
+            form.fields['childcare_address_declare'].widget.attrs.update(
+                {
+                    'data_target': 'childcare_address_' + str(index + 1),
+                    'aria-controls': 'childcare_address_' + str(index + 1),
+                    'aria-expanded': 'false'
+                }
+            )
+
+        print('Break')
+
         context = {
             'application_id': application_id,
             'title': 'Review: Childcare address',
-            # 'form': '',
             'rows': [
                 {
-                    'id': 'work_location_bool',
+                    'id': 'address_to_be_provided',
                     'name': "Do you know where you'll be working?",
-                    'info': work_location_bool
+                    'info': work_location_bool,
+                    'declare': where_you_will_work_form['address_to_be_provided_declare'],
+                    'comments': where_you_will_work_form['address_to_be_provided_comments']
                 },
                 {
                     'id': 'work_at_home_bool',
@@ -89,10 +77,10 @@ class NannyChildcareAddressSummary(View):
                 {
                     'id': 'home_address_locations',
                     'name': 'Childcare address',
-                    'info': home_address_locations
+                    'info': home_address_locations,
+                    'formset': childcare_address_formset
                 },
             ]
-
         }
 
         return context
