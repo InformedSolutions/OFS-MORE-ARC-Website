dadf81a36ae987c545def48a48a1df0254e20859
Thomas-95is <thomas.stokes@informed.com>
2018-11-05 15:25:52 +0000

    nanny_childcare_address.py: Add logic for False address_to_be_provided

--- arc_application/views/nanny_views/nanny_childcare_address.py
+++ arc_application/views/nanny_views/nanny_childcare_address.py
@@ -17,6 +17,29 @@ class NannyChildcareAddressSummary(NannyARCFormView):
         nanny_actions = NannyGatewayActions()
         nanny_application = nanny_actions.read('application',
                                                params={'application_id': application_id}).record
+
+        work_location_bool = nanny_application['address_to_be_provided']
+
+        where_you_will_work_form, childcare_address_formset = self.get_forms()
+
+        # If applicant has opted to provide addresses at a later time, skip remainder of summary page - load only this.
+        if not work_location_bool:
+            context = {
+                'application_id': application_id,
+                'title': 'Review: Childcare address',
+                'rows': [
+                    {
+                        'id': 'address_to_be_provided',
+                        'name': "Do you know where you'll be working?",
+                        'info': work_location_bool,
+                        'declare': where_you_will_work_form['address_to_be_provided_declare'] if hasattr(self, 'request') else '',
+                        'comments': where_you_will_work_form['address_to_be_provided_comments']
+                    },
+                ]
+            }
+
+            return context
+
         home_address_info = nanny_actions.read('applicant-home-address',
                                                params={'application_id': application_id,
                                                        'current_address': True}).record
@@ -24,7 +47,6 @@ class NannyChildcareAddressSummary(NannyARCFormView):
                                                params={'application_id': application_id,
                                                        'childcare_address': True}).record
 
-        work_location_bool = nanny_application['address_to_be_provided']
         if home_address_info == childcare_address_info:
             work_at_home_bool = 'Yes'
         else:
@@ -35,8 +57,6 @@ class NannyChildcareAddressSummary(NannyARCFormView):
                                                         params={'application_id': application_id}).record
         else:
             home_address_locations = {}
-
-        where_you_will_work_form, childcare_address_formset = self.get_forms()
 
         context = {
             'application_id': application_id,
