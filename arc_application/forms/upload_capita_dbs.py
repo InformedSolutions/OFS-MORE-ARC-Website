64d83d57669b546f5a236f079d2021b0e3df33aa
Thomas-95is <thomas.stokes@informed.com>
2019-02-12 15:35:09 +0000

    upload_capita_dbs.py: Add validation rules for file upload.

--- arc_application/forms/upload_capita_dbs.py
+++ arc_application/forms/upload_capita_dbs.py
@@ -1,10 +1,24 @@
-
-from django.forms import FileField
+from django.forms import FileField, ValidationError
 from govuk_forms.forms import GOVUKForm
 
 
 class UploadCapitaDBSForm(GOVUKForm):
-    capita_list_file = FileField(required=True,
-                                 error_messages={
-                                     'required': 'No file chosen'
-                                 })
+    error_summary_title = 'There was a problem'
+
+    valid_extensions = (
+        '.xlxs',
+        '.csv'
+    )
+
+    capita_list_file = FileField(allow_empty_file=True, required=False, label='Upload a file')
+
+    def clean_capita_list_file(self):
+        capita_list_file = self.data['capita_list_file']
+
+        if capita_list_file is None:
+            raise ValidationError('No file chosen')
+
+        if not any([capita_list_file.endswith(ext) for ext in self.valid_extensions]):
+            raise ValidationError('The file must be .csv or .xlxs')
+
+        return capita_list_file
