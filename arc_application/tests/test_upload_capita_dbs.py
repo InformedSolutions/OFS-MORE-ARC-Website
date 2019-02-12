7345c80ba9f457496af9ba9bdbae34051ec8e8f4
Thomas-95is <thomas.stokes@informed.com>
2019-02-12 12:41:59 +0000

    test_upload_capita_dbs.py: Increase test coverage.

--- arc_application/tests/test_upload_capita_dbs.py
+++ arc_application/tests/test_upload_capita_dbs.py
@@ -1,9 +1,31 @@
+from arc_application.forms import UploadCapitaDBSForm
+
+from django.conf import settings
+from django.contrib.auth.models import Group, User
+from django.forms import ValidationError
 from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
 
 
 class UploadCapitaDBSFunctionalTests(TestCase):
+    def setUp(self):
+        # Create ARC user and login.
+        self.arc_user = User.objects.create_user(
+            username='governor_tARCin',
+            email='test@test.com',
+            password='my_secret'
+        )
+
+        g = Group.objects.create(name=settings.ARC_GROUP)
+        g.user_set.add(self.arc_user)
+
+        self.client.login(username='governor_tARCin', password='my_secret')
+
     def test_can_render_capita_dbs_list_upload_page(self):
-        self.skipTest('FunctionalityNotImplemented')
+        response = self.client.get(reverse('Upload-Capita-DBS'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='upload-capita-dbs.html')
 
     def test_page_loads_with_date_and_filename_of_previous_upload(self):
         self.skipTest('FunctionalityNotImplemented')
@@ -18,7 +40,7 @@ class UploadCapitaDBSFunctionalTests(TestCase):
         self.skipTest('FunctionalityNotImplemented')
 
     def test_cc_user_cannot_see_nav_bar_link(self):
-        self.skipTest('FunctionalityNotImplemented')
+        self.skipTest('testNotImplemented')
 
     def test_cc_user_accessing_capita_dbs_list_upload_page_raises_access_denied(self):
         self.skipTest('FunctionalityNotImplemented')
@@ -26,7 +48,10 @@ class UploadCapitaDBSFunctionalTests(TestCase):
 
 class CapitaDBSListUploadFormTests(SimpleTestCase):
     def test_no_file_selected_raises_error(self):
-        self.skipTest('FunctionalityNotImplemented')
+        form = UploadCapitaDBSForm(data={'capita_list_file': ''})
+
+        with self.assertRaisesMessage(ValidationError, 'No file chosen'):
+            form.fields['capita_list_file'].clean('')
 
     def test_invalid_file_upload_raises_error(self):
         self.skipTest('FunctionalityNotImplemented')
