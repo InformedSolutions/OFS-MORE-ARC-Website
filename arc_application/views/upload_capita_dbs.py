7da394012c8a9f9dce35223771935e47644cf773
Thomas-95is <thomas.stokes@informed.com>
2019-02-14 14:18:09 +0000

    upload_capita_dbs.py: Handle batch overwrite from file upload.

--- arc_application/views/upload_capita_dbs.py
+++ arc_application/views/upload_capita_dbs.py
@@ -1,30 +1,64 @@
 import csv
+from datetime import datetime
 import json
 
 from arc_application.decorators import group_required
+from arc_application.models import CapitaDBSFile
 from arc_application.services import dbs_api
 from arc_application.forms import UploadCapitaDBSForm
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import InternalError
 from django.shortcuts import render
 
 
 def __format_last_upload():
+    try:
+        capita_dbs_file = CapitaDBSFile.objects.get(id=1)
+        return '%s (%s)' % (capita_dbs_file.filename, capita_dbs_file.date_uploaded.strftime('%d/%m/%Y'))
+
+    except CapitaDBSFile.DoesNotExist:
+        return 'File yet to be uploaded.'
+
+
+def __update_last_upload(filename):
+    date = datetime.now().date()
+    queryset = CapitaDBSFile.objects.all()
+
+    if queryset.exists():
+        queryset.delete()
+
+    CapitaDBSFile.objects.create(filename=filename, date_uploaded=date)
     return None
 
 
-def __handle_file_upload(f):
+def __format_csv_data(filename):
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, fieldnames=('date_of_birth', 'certificate_number', 'certificate_information', 'date_of_issue'))
+        next(reader)  # Skip header line of csv file.
+        data = [row for row in reader]
 
-    if f.lower().endswith('.csv'):
-        with open(f, 'r') as csvfile:
-            reader = csv.DictReader(csvfile, fieldnames=('date_of_birth', 'certificate_number', 'certificate_information', 'date_of_issue'))
+    csvfile.close()
+    return data
 
-            next(reader)  # Skip first line of csv file.
 
-            # data = json.dumps([row for row in reader])
-            data = json.dumps([next(reader) for row in range(10)])
-            print (data)
+def __handle_file_upload(filename):
+    data = __format_csv_data(filename=filename)
+
+    response = dbs_api.batch_overwrite(data)
+
+    if response.status_code == 201:
+        return
+
+    elif response.status_code == 400:
+        response_text = json.loads(response.text)
+        raise ValidationError(response_text)
+
+    else:
+        response_text = json.loads(response.text)
+        raise InternalError('The DBS API returned a %i status code. Response text: %s' % response.status_code, response_text)
 
 
 @login_required
@@ -38,7 +72,13 @@ def upload_capita_dbs(request):
         form = UploadCapitaDBSForm(request.POST)
 
         if form.is_valid():
-            __handle_file_upload(request.POST['capita_list_file'])
+            try:
+                __handle_file_upload(request.POST['capita_list_file'])
+                __update_last_upload(request.POST['capita_list_file'])
+            except ValidationError:
+                form.add_error('capita_list_file', 'There was an error with the file you tried to upload. Check the file and try again')
+            except InternalError:
+                form.add_error('capita_list_file', 'We couldn\'t upload your new list. Try again')
 
     context = {
         'form': form,
