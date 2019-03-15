1fbe5c7dd5957ac7ba938b6e6b56dfa2eccf3820
katej321 <klj525@york.ac.uk>
2019-03-15 11:39:42 +0000

    Rendering previous registration page

--- arc_application/views/nanny_views/nanny_previous_registration.py
+++ arc_application/views/nanny_views/nanny_previous_registration.py
@@ -8,63 +8,66 @@ from .nanny_form_view import FormView
 
 
 class NannyPreviousRegistrationView(FormView):
+    form_class = PreviousRegistrationDetailsForm
+    template_name = 'nanny_add_previous_registration.html'
 
-        def get(self, request, *args, **kwargs):
-            self.application_id = request.GET["id"]
-            form = PreviousRegistrationDetailsForm
+    def get_initial(self):
+        initial = super().get_initial()
+        application_id = self.request.GET["id"]
+
+        api_response = NannyGatewayActions().read('previous-registration-details',
+                                                  params={'application_id': application_id})
+        if api_response.status_code == 200:
+            previous_response = api_response.record
+            initial['previous_registration'] = previous_response['previous_registration']
+            initial['individual_id'] = previous_response['individual_id']
+            initial['five_years_in_UK'] = previous_response['five_years_in_UK']
+        return initial
+
+    def get_context_data(self, **kwargs):
+        application_id = self.request.GET["id"]
+        context = {
+            'application_id': application_id,
+        }
+
+        return super(NannyPreviousRegistrationView, self).get_context_data(**context)
+
+    def post(self, request, *args, **kwargs):
+        application_id = request.POST["id"]
+        form = PreviousRegistrationDetailsForm(request.POST)
+
+        if form.is_valid():
+            previous_registration = form.cleaned_data.get('previous_registration')
+            individual_id = form.cleaned_data.get('individual_id')
+            five_years_in_uk = form.cleaned_data.get('five_years_in_UK')
+
+            api_response = NannyGatewayActions().read('previous-registration-details',
+                                                      params={'application_id': application_id})
+
+            if api_response.status_code == 200:
+                previous_registration_record = api_response.record
+                previous_registration_record['previous_registration'] = self.request.POST['previous_registration']
+                previous_registration_record['individual_id'] = self.request.POST['individual_id']
+                previous_registration_record['five_years_in_UK'] = self.request.POST['five_years_in_UK']
+
+                NannyGatewayActions().put('previous-registration-details', params=previous_registration_record)
+            else:
+                previous_registration_new = {}
+                previous_registration_new['application_id'] = application_id
+                previous_registration_new['previous_registration'] = self.request.POST['previous_registration']
+                previous_registration_new['individual_id'] = self.request.POST['individual_id']
+                previous_registration_new['five_years_in_UK'] = self.request.POST['five_years_in_UK']
+
+                response = NannyGatewayActions().create('previous-registration-details',
+                                                        params=previous_registration_new)
+
+            redirect_link = '/nanny/personal-details/'
+            return HttpResponseRedirect(settings.URL_PREFIX + redirect_link + '?id=' + application_id)
+
+        else:
             variables = {
                 'form': form,
-                'application_id': self.application_id,
+                'application_id': application_id,
             }
 
-            return render(request, 'nanny_add_previous_registration.html', variables)
-
-        def post(self, request, *args, **kwargs):
-            application_id = request.POST["id"]
-            form = PreviousRegistrationDetailsForm(request.POST)
-
-            if form.is_valid():
-                previous_registration = form.cleaned_data.get('previous_registration')
-                individual_id = form.cleaned_data.get('individual_id')
-                five_years_in_uk = form.cleaned_data.get('five_years_in_UK')
-
-                api_response = NannyGatewayActions().read('previous-registration-details',
-                                                          params={'application_id': application_id})
-
-                if api_response.status_code == 200:
-                    previous_registration_record = api_response.record
-                    previous_registration_record['previous_registration'] = self.request.POST['previous_registration']
-                    previous_registration_record['individual_id'] = self.request.POST['individual_id']
-                    previous_registration_record['five_years_in_UK'] = self.request.POST['five_years_in_UK']
-
-                    NannyGatewayActions().put('previous-registration-details', params=previous_registration_record)
-                else:
-                    previous_registration_new = {}
-                    previous_registration_new['application_id'] = application_id
-                    previous_registration_new['previous_registration'] = self.request.POST['previous_registration']
-                    previous_registration_new['individual_id'] = self.request.POST['individual_id']
-                    previous_registration_new['five_years_in_UK'] = self.request.POST['five_years_in_UK']
-
-                    response =  NannyGatewayActions().create('previous-registration-details',
-                                                             params=previous_registration_new)
-
-                redirect_link = '/nanny/personal-details/'
-                return HttpResponseRedirect(settings.URL_PREFIX + redirect_link + '?id=' + application_id)
-
-            else:
-                variables = {
-                    'form': form,
-                    'application_id': application_id,
-                }
-
-                return render(request, 'nanny_add_previous_registration.html', context=variables)
-
-        def get_initial(self):
-            initial = super().get_initial()
-            api_response = NannyGatewayActions().read('previous-registration-details', params={'application_id': self.application_id})
-            if api_response.status_code == 200:
-                previous_response = api_response.record
-                initial['previous_registration'] = previous_response['previous_registration']
-                initial['individual_id'] = previous_response['individual_id']
-                initial['five_years_in_UK'] = previous_response['five_years_in_UK']
-            return initial
+            return render(request, 'nanny_add_previous_registration.html', context=variables)
