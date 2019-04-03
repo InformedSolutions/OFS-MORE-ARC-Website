afadcfe5323475814df27e6f820a75286e575cdd
James Cruddas <james.cruddas@informed.com>
2019-04-03 13:39:34 +0100

    Update export function for home addresses

--- arc_application/messaging/application_exporter.py
+++ arc_application/messaging/application_exporter.py
@@ -75,10 +75,6 @@ class ApplicationExporter:
         applicant_previous_addresses = PreviousAddress.objects.filter(person_type='APPLICANT', person_id=application_id)
         export['applicant_previous_addresses'] = serializers.serialize('json', list(applicant_previous_addresses))
 
-        childcare_address = ApplicantHomeAddress.objects.filter(personal_detail_id=applicant_personal_details.first(),
-                                                                    childcare_address=True)
-        export['childcare_address'] = serializers.serialize('json', list(childcare_address))
-
         child_in_home = ChildInHome.objects.filter(application_id=application_id)
         export['child_in_home'] = serializers.serialize('json', list(child_in_home))
 
@@ -224,12 +220,20 @@ class ApplicationExporter:
         :param export_object: The export object to be sent to SQS
         """
         try:
-            decoded_home_address = json.loads(export_object['applicant_home_address'])[0]
+            decoded_home_addresses = json.loads(export_object['applicant_home_address'])
+
+            decoded_home_address = None
+
+            # Find current address
+            for address in decoded_home_addresses:
+                if address['fields']['current_address']:
+                    decoded_home_address = address
+
             decoded_personal_details = json.loads(export_object['applicant_personal_details'])[0]
             decoded_home_address['fields']['moved_in_day'] = decoded_personal_details['fields']['moved_in_day']
             decoded_home_address['fields']['moved_in_month'] = decoded_personal_details['fields']['moved_in_month']
             decoded_home_address['fields']['moved_in_year'] = decoded_personal_details['fields']['moved_in_year']
-            export_object['applicant_home_address'] = json.dumps([decoded_home_address])
+            export_object['applicant_home_address'] = json.dumps([decoded_home_addresses])
         except:
             pass
 
