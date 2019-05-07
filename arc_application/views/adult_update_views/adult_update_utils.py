4b327fedd628cee07ab23bfcadfb20393fc3ec19
Thomas-95is <thomas.stokes@informed.com>
2019-05-07 14:31:56 +0100

    adult_update_utils.py: Update to handle single adult only.

--- arc_application/views/adult_update_views/adult_update_utils.py
+++ arc_application/views/adult_update_views/adult_update_utils.py
@@ -3,22 +3,22 @@ from arc_application.services.db_gateways import HMGatewayActions
 from .adult_update_summary import load_json
 
 
-def get_adult_update_summary_variables(application_id, adult_id):
+def get_adult_update_summary_variables(adult_id):
     """
     Method to format the output from load_json, whose output is an ordered list of tables for adults' updates tables.
     This method essentially splits out these tables per adult so that only a single adult's information is returned.
     """
-    all_adult_tables = load_json(application_id)
+    dpa_auth_id = HMGatewayActions().read('adult', params={'adult_id': adult_id}).record['token_id']
+    application_reference = HMGatewayActions().read('dpa-auth', params={'token_id': dpa_auth_id}).record['URN']
 
-    application_reference = HMGatewayActions().read('dpa-auth', params={'token_id': application_id}).record['URN']
-    single_adult_tables = [table for table in all_adult_tables if table[0]['id'] == adult_id]
+    adult_tables = load_json(adult_id)
 
     variables = {
         'application_reference': application_reference,
-        'json': single_adult_tables
+        'json': adult_tables
     }
 
-    if len(single_adult_tables) == 0:
-        raise ValueError("Adult with adult_id: %s not found in summary data for application_id: %s" % (adult_id, application_id))
+    if len(adult_tables) == 0:
+        raise ValueError("Adult with adult_id: %s not found in summary data." % adult_id)
     else:
         return variables
