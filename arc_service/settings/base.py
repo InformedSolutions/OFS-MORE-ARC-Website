16e30f6142b0053e35d760c785b8f959dcdd7590
Thomas-95IS <thomas.stokes@informed.com>
2019-09-05 11:44:02 +0100

    contenttypes: Remove redundant app.

--- arc_service/settings/base.py
+++ arc_service/settings/base.py
@@ -44,7 +44,6 @@ BUILTIN_APPS = [
     'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
