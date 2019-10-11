0e0ebfe94a7ceee7a07417b32959a9cacbf96c07
Tomasz Muszczyk <nevalive@gmail.com>
2019-10-11 12:05:55 +0200

    added linking screen

---
+++ arc_application/views/childminder_views/personal_details_individual_lookup.py
@@ -0,0 +1,42 @@
+import logging
+import datetime
+
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from ...decorators import group_required, user_assigned_application
+from ...models import ApplicantPersonalDetails, ApplicantName, ApplicantHomeAddress, Arc, Application, PreviousName
+from ...review_util import request_to_comment, save_comments, redirect_selection
+from ...forms.childminder_forms.form import PersonalDetailsForm, PreviousRegistrationDetails
+
+log = logging.getLogger()
+
+
+@login_required
+@group_required(settings.ARC_GROUP, raise_exception=True)
+@user_assigned_application
+def personal_details_individual_lookup(request):
+
+    application_id_local = request.GET["id"]
+    application_record = Application.objects.get(application_id=application_id_local)
+    personal_details_record = ApplicantPersonalDetails.objects.get(application_id=application_id_local)
+    name_record = ApplicantName.objects.get(personal_detail_id=personal_details_record)
+    home_address_record = ApplicantHomeAddress.objects.get(personal_detail_id=personal_details_record,
+                                                           current_address=True)
+    childcare_address_record = ApplicantHomeAddress.objects.get(personal_detail_id=personal_details_record,
+                                                                childcare_address=True)
+
+    variables = {
+        'first_name': name_record.first_name,
+        'last_name': name_record.last_name,
+        'date_of_birth': personal_details_record.date_of_birth,
+        'street_line1': home_address_record.street_line1,
+        'street_line2': home_address_record.street_line2,
+        'town': home_address_record.town,
+        'postcode': home_address_record.postcode,
+        'application_id': application_id_local,
+    }
+
+    return render(request, 'childminder_templates/personal-details-individual-lookup.html', variables)
