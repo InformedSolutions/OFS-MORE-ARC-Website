e91b38d7a2d0ec6c25fc1c2c7429dbaf5f9620c0
trichmond-IS <thomas.richmond@informed.com>
2020-03-02 15:27:25 +0000

    imporve logic around previous address and lived outside uk for childminder

--- arc_application/views/childminder_views/personal_details_addresses.py
+++ arc_application/views/childminder_views/personal_details_addresses.py
@@ -201,7 +201,8 @@ def postcode_submission(request):
             line2 = selected_address['line2']
             town = selected_address['townOrCity']
             county = ''
-            country = 'United Kingdom'
+            if lived_abroad:
+                country = selected_address['country'] if selected_address.get('country') else 'United Kingdom'
             postcode = selected_address['postcode']
 
         moved_in_day = int(request.POST['moved_in_date_0'])
