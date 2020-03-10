4864787737f0986534cc1f0a5c8ef0945b22904f
trichmond-IS <thomas.richmond@informed.com>
2020-03-10 14:02:50 +0000

    wip - applications in queue

--- arc_application/views/applications_summary.py
+++ arc_application/views/applications_summary.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from ..models import Application
 from .. services.db_gateways import NannyGatewayActions, HMGatewayActions
 from django.conf import settings
+from datetime import datetime, timedelta
 
 # Initiate logging
 log = logging.getLogger()
@@ -106,6 +107,46 @@ class ApplicationsSummaryView(View):
         household_member_data['non_draft_applications'] = total_applications - household_member_data["draft_applications"]
 
         return household_member_data
+
+    def extract_applications_in_queue(self):
+        """
+        function to list any application that has a status of SUBMITTED, grouped by date and by application.
+        :return: list of adult, childminder and nanny records
+        """
+        now = datetime.now()
+        initial_date = datetime(2020, 2, 19, 0, 0)
+        delta = timedelta(days=1)
+        adult_response = HMGatewayActions().list('adult', params={"adult_status": 'SUBMITTED'})
+        nanny_response = NannyGatewayActions().list('application', params={"application_status": 'SUBMITTED'})
+        cm_applications = list(Application.objects.filter(application_status='SUBMITTED', ))
+        apps_in_queue = {}
+        while initial_date <= now:
+            cm_apps = 0
+            adult_apps = 0
+            nanny_apps = 0
+            for item in cm_applications:
+                if item.date_submitted == initial_date:
+                    cm_apps += 1
+            if adult_response.status_code == 200:
+                adults_submitted = adult_response.record
+                for adult in adults_submitted:
+                    if adult['date_resubmitted'] is None and datetime.strptime(
+                            adult['date_submitted'], "%Y-%m-%dT%H:%M:%S.%fZ").date() == initial_date.date():
+                        adult_apps += 1
+                    elif adult['date_resubmitted'] is not None and datetime.strptime(
+                            adult['date_resubmitted'], "%Y-%m-%dT%H:%M:%S.%fZ").date() == initial_date.date():
+                        adult_apps += 1
+            if nanny_response.status_code == 200:
+                nannies_submitted = nanny_response.record
+                for nanny in nannies_submitted:
+                    if nanny['date_submitted'] == initial_date and not None:
+                        nanny_apps += 1
+
+            apps_in_queue['Date'] = initial_date.isoformat()
+            apps_in_queue['Date'] = {'Childminder': cm_apps, 'Adult': adult_apps, 'Nanny': nanny_apps}
+            initial_date += delta
+
+        return (apps_in_queue)
               
     def get_context_data(self):
         """
@@ -117,6 +158,7 @@ class ApplicationsSummaryView(View):
         nanny_data = self.get_nanny_data()
         context['enable_hm'] = False
         hm_data = {}
+        apps_in_queue = self.extract_applications_in_queue()
         if settings.ENABLE_HM:
             hm_data = self.get_hm_data()
             context['enable_hm'] = True
